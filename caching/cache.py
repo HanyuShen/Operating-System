cc0cda124e3d72d31ec26b3d2c16177f1fff21c0
hanyu shen <hanyu.shen@student.manchester.ac.uk>
2021-04-16 16:43:44 +0100

    coursework1

--- caching/cache.py
+++ caching/cache.py
@@ -14,6 +14,22 @@ class CyclicCache(Memory):
 
     def __init__(self):
         super().__init__()
+        self.cache = [(None,None),(None,None),(None,None),(None,None)]
+
+    def lookup(self,address):
+        for cache_address in self.cache:
+            if address == cache_address:
+                self.hit_count -= 1
+                return super().lookup(address)
+    
+        if len(self.cache) > 3:
+            del self.cache[0]
+            self.cache.append(address)
+        else:
+            self.cache.append(address)
+
+        return super().lookup(address)
+
 
 
 class LRUCache(Memory):
@@ -27,3 +43,24 @@ class LRUCache(Memory):
 
     def __init__(self):
         super().__init__()
+        self.cache = [(None, None), (None, None), (None, None), (None, None)]
+
+    def lookup(self, address):
+        addresses = [x[0] for x in self.cache]
+        cacheData = [x[1] for x in self.cache]
+        if address not in addresses:
+            self.cache = self.cache[1:]
+            self.cache.append((address, super().lookup(address)))
+            addresses = [x[0] for x in self.cache]
+            cacheData = [x[1] for x in self.cache]
+            return cacheData[addresses.index(address)]
+        else:
+            index = addresses.index(address)
+            self.cache = self.cache[0:index] + self.cache[index+1:]
+            self.cache.append((address, cacheData[index]))
+            return cacheData[addresses.index(address)]+"(not hit)"
+
+
+     
+
+
